a3df3d2796669de65f7a3f46db59116618bbe82d
R4wdha19 <rweedh@hotmail.com>
2022-10-29 23:20:50 -0700

    Updated calss mark

--- src/menuForStudents/Mark.java
+++ src/menuForStudents/Mark.java
@@ -4,55 +4,25 @@ import java.io.Serializable;
 
 public class Mark implements Serializable {
 	private   int markOfAssignment1;
-//	private   int markOfAssignment2;
-//	private   int markOfAssignment3;
 	private   int markOfTest1;
-//	private   int markOfTest2;
-//	private   int markOfTest3;
-     
+
      
    //Getters and Setters
  	
  	public void setmarkOfAssignment1(int markOfAssignment1) {
  		this.markOfAssignment1 = markOfAssignment1;
  	}
-// 	
-// 	public void setmarkOfAssignment2(int markOfAssignment2) {
-// 		this.markOfAssignment2 = markOfAssignment2;
-// 	}
-// 	
-// 	public void setmarkOfAssignment3(int markOfAssignment3) {
-// 		this.markOfAssignment3 = markOfAssignment3;
-// 	}
+
  	public int getmarkOfAssignment1(){
 	 		return markOfAssignment1;
 	 	}
-//	 public int getmarkOfAssignment2(){
-//		return markOfAssignment2;
-//	}
-//	public int getmarkOfAssignment3(){
-//		return markOfAssignment3;
-//	}
 
  	public void setmarkOfTest1(int markOfTest1) {
  		this.markOfTest1 = markOfTest1;
  	}
-// 	public void setmarkOfTest2(int markOfTest2) {
-// 		this.markOfTest2 = markOfTest2;
-// 	}
-// 	public void setmarkOfTest3(int markOfTest3) {
-// 		this.markOfTest3 = markOfTest3;
-// 	}
+
  	   public int getmarkOfTest1(){
  		return markOfTest1;
  	}
-// 	   public int getmarkOfTest2(){
-// 		return markOfTest2;
-// 	}
-// 	
-// 	   public int getmarkOfTest3(){
-// 		return markOfTest3;
-// 	}
- 	  
  	   
 }
